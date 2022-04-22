b66993b2b7248a57424e83d1ccafd4c2ed6f9c8f
beowulf1416 <beowulf1416@gmail.com>
2022-04-22 19:44:42 +0800

    add validation to signup and signin modules

--- ui/saas/src/app/modules/anonymous/signin/signin.module.ts
+++ ui/saas/src/app/modules/anonymous/signin/signin.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './components/signin/signin.component';
@@ -11,6 +12,7 @@ import { SigninRoutingModule } from './signin-routing.module';
     SigninComponent
   ],
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     SigninRoutingModule
   ]
