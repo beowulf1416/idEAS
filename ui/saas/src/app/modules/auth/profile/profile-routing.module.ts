ed12370915fbd0eae68ee1a67e134104fae8058a
beowulf1416 <beowulf1416@gmail.com>
2022-05-01 14:34:36 +0800

    add route to profile

--- ui/saas/src/app/modules/auth/profile/profile-routing.module.ts
+++ ui/saas/src/app/modules/auth/profile/profile-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from 'src/app/guards/auth.guard';
 import { PasswordComponent } from './components/password/password.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
   {
     path: '',
     canActivate: [AuthGuard],
+    component: ProfileComponent
+  },
+  {
+    path: 'password',
     component: PasswordComponent
   }
 ];
