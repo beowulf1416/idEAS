d2eab66d4f705bb7e9c944525209823301819555
beowulf1416 <beowulf1416@gmail.com>
2022-05-01 15:03:55 +0800

    add sp to change user password

--- data/db/sql/iam/create_procs.sql
+++ data/db/sql/iam/create_procs.sql
@@ -3,6 +3,7 @@ set schema 'iam';
 
 \ir procedures/users/user_add.sql
 \ir procedures/users/user_set_active.sql
+\ir procedures/users/user_set_password.sql
 \ir procedures/users/user_authenticate.sql
 \ir procedures/users/user_get_by_email.sql
 \ir procedures/users/user_get_id.sql
