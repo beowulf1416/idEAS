af63c9d583de383c35c4ea6d3445352044ae548d
beowulf1416 <beowulf1416@gmail.com>
2022-05-01 15:03:55 +0800

    add sp to change user password

---
+++ data/db/sql/iam/procedures/users/user_set_password.sql
@@ -0,0 +1,14 @@
+create or replace procedure user_set_password (
+    p_user_id iam.users.id%type,
+    p_pw iam.users.password%type
+)
+language plpsql
+as $$
+begin
+    update iam.users set
+        pw = public.crypt(p_pw, public.gen_salt('md5'))
+    where
+        id = p_user_id
+    ;
+end
+$$;
