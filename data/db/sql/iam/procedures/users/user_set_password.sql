e984f1f9a248b659d942f3b909ab23ce0b84ebd6
beowulf1416 <beowulf1416@gmail.com>
2022-05-01 19:34:54 +0800

    fix incorrect sp

--- data/db/sql/iam/procedures/users/user_set_password.sql
+++ data/db/sql/iam/procedures/users/user_set_password.sql
@@ -1,8 +1,8 @@
 create or replace procedure user_set_password (
     p_user_id iam.users.id%type,
-    p_pw iam.users.password%type
+    p_pw iam.users.pw%type
 )
-language plpsql
+language plpgsql
 as $$
 begin
     update iam.users set
