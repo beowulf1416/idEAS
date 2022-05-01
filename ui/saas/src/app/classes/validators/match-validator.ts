690b21f1236a07e0ca3699a2d5ac7bda1d46a972
beowulf1416 <beowulf1416@gmail.com>
2022-05-01 20:54:11 +0800

    refactor validators

---
+++ ui/saas/src/app/classes/validators/match-validator.ts
@@ -0,0 +1,10 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function matchValidator(control1: string, control2: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value1 = control.get(control1)?.value;
+      const value2 = control.get(control2)?.value;
+  
+      return value1 == value2 ? null : { identical: true };
+    }
+}
---
+++ ui/saas/src/app/classes/validators/pattern-validator.spec.ts
@@ -0,0 +1,7 @@
+import { PatternValidator } from './pattern-validator';
+
+describe('PatternValidator', () => {
+  it('should create an instance', () => {
+    expect(new PatternValidator()).toBeTruthy();
+  });
+});
