a76300335dc8bc5f4d282feacbcc55f1d349275d
beowulf1416 <beowulf1416@gmail.com>
2022-05-02 21:39:41 +0800

    add sp to toggle user-tenant active status. modify user_add sp to add new user to default tenant.

--- data/db/sql/iam/procedures/users/user_add.sql
+++ data/db/sql/iam/procedures/users/user_add.sql
@@ -8,6 +8,8 @@ create or replace procedure user_add (
 )
 language plpgsql
 as $$
+declare
+    t_tenant_id iam.user_tenants.tenant_id%type;
 begin
     insert into iam.users (
         id,
@@ -17,6 +19,13 @@ begin
         p_user_id,
         p_email,
         public.crypt(p_pw, public.gen_salt('md5'))
+    );
+
+    -- add user to default tenant
+    t_tenant_id := tenants.tenant_default_id();
+    call iam.assign_user_to_tenant(
+        p_user_id,
+        t_tenant_id
     );
 end
 $$;
