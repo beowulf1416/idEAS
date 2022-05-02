a4269778caaa82209c8d48abcd81cf02e56ea4f3
beowulf1416 <beowulf1416@gmail.com>
2022-05-02 21:24:28 +0800

    modify password component

--- ui/saas/src/app/modules/auth/profile/components/password/password.component.ts
+++ ui/saas/src/app/modules/auth/profile/components/password/password.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { matchValidator } from 'src/app/classes/validators/match-validator';
+import { patternValidator } from 'src/app/classes/validators/pattern-validator';
+
 import { TitleService } from 'src/app/services/title.service';
 import { PasswordService } from '../../services/password.service';
 
@@ -12,11 +15,16 @@ export class PasswordComponent implements OnInit {
 
   passwordForm = new FormGroup({
     pw1: new FormControl('', [
-
+      Validators.required,
+      Validators.minLength(8),
+      patternValidator(new RegExp("[A-Z]{2,}"), { upper: true }),
+      patternValidator(new RegExp("[0-9]{2,}"), { numeric: true })
     ]),
     pw2: new FormControl('', [
-
+      Validators.required
     ])
+  }, {
+    validators: matchValidator("pw1", "pw2")
   });
 
   constructor(
@@ -33,12 +41,18 @@ export class PasswordComponent implements OnInit {
     return this.passwordForm.get("pw1");
   }
 
+  get pw2() {
+    return this.passwordForm.get("pw2");
+  }
+
   submit() {
     console.log("PasswordComponent::submit()");
-    this.password_service.change(
-      this.passwordForm.get("pw1")?.value
-    ).subscribe(r => {
-      console.log(r);
-    });
+    if (this.passwordForm.valid) {
+      this.password_service.change(
+        this.passwordForm.get("pw1")?.value
+      ).subscribe(r => {
+        console.log(r);
+      });
+    }
   }
 }
