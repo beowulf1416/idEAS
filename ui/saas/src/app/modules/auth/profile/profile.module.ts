d03f3124ff6b37673e7ab7e532de3b7d7ffc9ce0
beowulf1416 <beowulf1416@gmail.com>
2022-05-02 21:24:28 +0800

    modify password component

--- ui/saas/src/app/modules/auth/profile/profile.module.ts
+++ ui/saas/src/app/modules/auth/profile/profile.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { PasswordComponent } from './components/password/password.component';
@@ -14,6 +15,7 @@ import { ProfileComponent } from './components/profile/profile.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ProfileRoutingModule
   ]
 })
