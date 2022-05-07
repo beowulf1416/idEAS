26a90f2aa8f3a70db5f6dc5c051d3916cc1c6171
beowulf1416 <beowulf1416@gmail.com>
2022-05-07 22:00:00 +0800

    add procs for accounting schema

--- data/db/sql/20_create_procs.sql
+++ data/db/sql/20_create_procs.sql
@@ -4,3 +4,4 @@
 \ir common/create_procs.sql
 \ir tenants/create_procs.sql
 \ir iam/create_procs.sql
+\ir accounting/create_procs.sql
---
+++ data/db/sql/accounting/create_procs.sql
@@ -0,0 +1,7 @@
+set schema 'accounting';
+
+
+\ir procedures/tenants/tenant_add.sql
+
+\ir procedures/accounts/account_add.sql
+\ir procedures/accounts/account_set_active.sql
