b8e2dff074321f867c5875d23d8d1563a83ffb6c
beowulf1416 <beowulf1416@gmail.com>
2022-05-15 20:34:49 +0800

    fix incorrect sp

--- data/db/sql/iam/procedures/user_tenants/user_tenants_set_active.sql
+++ data/db/sql/iam/procedures/user_tenants/user_tenants_set_active.sql
@@ -1,9 +1,9 @@
 create or replace procedure user_tenants_set_active (
     p_user_id iam.user_tenants.user_id%type,
     p_tenant_id iam.user_tenants.tenant_id%type,
-    p_active iam.user.tenants.active%type
+    p_active iam.user_tenants.active%type
 )
-language plpsql
+language plpgsql
 as $$
 begin
     update iam.user_tenants set
