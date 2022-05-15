2281875728b1fd05e749a6b3a5607e9852185111
beowulf1416 <beowulf1416@gmail.com>
2022-05-15 20:34:20 +0800

    move csv folder. modify db init.

--- data/db/sql/common/init.sql
+++ data/db/sql/common/init.sql
@@ -1,31 +1,5 @@
-\copy common.countries from '../../csv/countries.csv' with csv header
+\cd docker-entrypoint-initdb.d
 
-\copy common.currencies from '../../csv/currencies.csv' with csv header
+\copy common.countries from 'common/csv/countries.csv' with csv header
 
--- copy common.countries (
---     official_name_en,
---     iso3166_1_alpha_2,
---     iso3166_1_alpha_3,
---     iso3166_1_numeric,
---     iso4217_currency_alphabetic_code,
---     iso4217_currency_country_name,
---     iso4217_currency_minor_unit,
---     iso4217_currency_name,
---     iso4217_currency_numeric_code
--- )
--- from '../../csv/countries.csv'
--- delimiter ','
--- csv
--- header
--- ;
-
--- copy common.currencies (
---     currency_id,
---     name,
---     symbol
--- )
--- from '../../csv/currencies.csv'
--- delimiter ','
--- csv
--- header
--- ;
+\copy common.currencies from 'common/csv/currencies.csv' with csv header
