f84d17f8fcd65326e9bebab0ff404f07fa2953ed
beowulf1416 <beowulf1416@gmail.com>
2022-05-23 22:45:08 +0800

    modify endpoint urls. add /tenants/user/active endpoint.

--- services/api/src/endpoints/tenants.rs
+++ services/api/src/endpoints/tenants.rs
@@ -20,12 +20,25 @@ struct TenantAddUserRequest {
 }
 
 
+#[derive(Serialize, Deserialize)]
+struct TenantUserActiveRequest {
+    pub tenant_id: Uuid,
+    pub user_id: Uuid,
+    pub active: bool
+}
+
+
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg
         .service(
-            web::resource("/add/user")
+            web::resource("/user/add")
                 .route(web::method(http::Method::OPTIONS).to(default_options))
                 .route(web::post().to(tenant_add_user))
+        )
+        .service(
+            web::resource("/user/active")
+                .route(web::method(http::Method::OPTIONS).to(default_options))
+                .route(web::post().to(tenant_user_set_active))
         )
     ;
 }
@@ -46,8 +59,8 @@ async fn tenant_add_user(
     match Tenants::from_request(&request).await {
         Ok(tenants_db) => {
             if let Err(e) = tenants_db.add_user(
-                &user_id,
-                &tenant_id
+                &tenant_id,
+                &user_id
             ).await {
                 error!("unable to add user to tenant: {:?}", e);
                 error_msg = format!("unable to add user to tenant: {}", e);
@@ -63,6 +76,54 @@ async fn tenant_add_user(
         Err(e) => {
             error!("unable to retrieve tenants db object: {:?}", e);
             error_msg = format!("unable to retrieve tenants db object: {}", e);
+        }
+    }
+
+
+    return HttpResponse::InternalServerError()
+        .json(ApiResponse {
+            status: ApiResponseStatus::Error,
+            message: error_msg,
+            data: None
+        });
+}
+
+
+
+// set user-tenant active status
+async fn tenant_user_set_active(
+    request: HttpRequest,
+    params: web::Json<TenantUserActiveRequest>
+) -> impl Responder {
+    info!("endpoints::tenants::tenant_user_set_active");
+
+    let mut error_msg = String::from("unable to set user-tenant active status");
+
+    let user_id = params.user_id.clone();
+    let tenant_id = params.tenant_id.clone();
+    let active = params.active.clone();
+
+    match Tenants::from_request(&request).await {
+        Ok(tenants_db) => {
+            if let Err(e) = tenants_db.set_active(
+                &tenant_id,
+                &user_id,
+                &active
+            ).await {
+                error!("unable to set user-tenant active status: {:?}", e);
+                error_msg = format!("unable to set user-tenant active status: {}", e);
+            } else {
+                return HttpResponse::Ok()
+                    .json(ApiResponse {
+                        status: ApiResponseStatus::Success,
+                        message: format!("successfully set user-tenant active status"),
+                        data: None
+                    });
+            }
+        }
+        Err(e) => {
+            error!("unable to set user-tenant active status: {:?}", e);
+            error_msg = format!("unable to set user-tenant active status: {}", e);
         }
     }
 
