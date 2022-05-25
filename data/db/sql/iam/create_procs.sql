418d7dbe803734ff271ba7645f9e9959736094e7
beowulf1416 <beowulf1416@gmail.com>
2022-05-25 21:47:42 +0800

    add /tenants/users/get endpoint

--- data/db/sql/iam/create_procs.sql
+++ data/db/sql/iam/create_procs.sql
@@ -18,6 +18,7 @@ set schema 'iam';
 \ir procedures/user_tenants/revoke_user_from_tenant.sql
 \ir procedures/user_tenants/user_tenants_get.sql
 \ir procedures/user_tenants/user_tenants_set_active.sql
+\ir procedures/user_tenants/user_tenants_list.sql
 
 \ir procedures/user_roles/assign_user_to_role.sql
 
