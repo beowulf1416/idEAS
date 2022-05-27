74f111015e743007cf9e38830c61730ffb35489e
beowulf1416 <beowulf1416@gmail.com>
2022-05-27 21:42:39 +0800

    add units of measure functions

--- data/db/sql/common/create_procs.sql
+++ data/db/sql/common/create_procs.sql
@@ -5,3 +5,18 @@ set schema 'common';
 
 \ir procedurs/currency_get.sql
 \ir procedures/currencies_get.sql
+
+\ir procedures/uom_length_get.sql
+\ir procedures/uom_lengths_get.sql
+
+\ir procedures/uom_area_get.sql
+\ir procedures/uom_areas_get.sql
+
+\ir procedures/uom_volume_get.sql
+\ir procedures/uom_volumes_get.sql
+
+\ir procedures/uom_mass_get.sql
+\ir procedures/uom_masses_get.sql
+
+\ir procedures/uom_quantity_get.sql
+\ir procedures/uom_quantities_get.sql
