4d9259a49d25efecae8d00adaa785ad382840c31
beowulf1416 <beowulf1416@gmail.com>
2022-08-29 21:06:18 +0800

    modify iam schema. add user invite and user registration tables.

--- data/sql/postgres/iam/tables.sql
+++ data/sql/postgres/iam/tables.sql
@@ -12,3 +12,7 @@ set schema 'iam';
 \ir tables/users.sql
 \ir tables/user_roles.sql
 \ir tables/user_domains.sql
+
+
+\ir tables/user_registration.sql
+\ir tables/user_invites.sql
--- data/sql/postgres/iam/tables/user_registration.sql
+++ data/sql/postgres/iam/tables/user_registration.sql
@@ -1,6 +1,9 @@
 create table user_registration (
     id uuid not null,
-    created datetime not null,
+    created timestamp without time zone not null default(now() at time zone 'utc'),
+
+    token varchar(20) not null,
+    email common.email_address,
 
     expires datetime not null,
     completed datetime not null,
