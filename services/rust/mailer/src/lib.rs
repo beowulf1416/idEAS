6d227fda3deec3d5950906baf3d25f04f4ff39b0
beowulf1416 <beowulf1416@gmail.com>
2022-08-31 21:16:18 +0800

    initial mailer code

--- services/rust/mailer/src/lib.rs
+++ services/rust/mailer/src/lib.rs
@@ -1,4 +1,38 @@
+use log::{
+    info,
+    error,
+    debug
+};
 
+
+
+pub struct Mailer {
+
+}
+
+
+impl Mailer {
+
+    pub fn new(
+        smtp_host: &str,
+        smtp_user: &str,
+        smtp_password: &str
+    ) -> Self {
+        return Self {
+
+        };
+    }
+
+    pub fn send(
+        &self,
+        from: &str,
+        to: &str,
+        subject: &str,
+        body: &str
+    ) {
+
+    }
+}
 
 #[cfg(test)]
 mod tests {
