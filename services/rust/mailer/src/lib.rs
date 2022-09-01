12ad025fb926222f0e1b56ebc91734b1ff4b0350
beowulf1416 <beowulf1416@gmail.com>
2022-09-01 22:16:30 +0800

    modify mailer code

--- services/rust/mailer/src/lib.rs
+++ services/rust/mailer/src/lib.rs
@@ -4,10 +4,25 @@ use log::{
     debug
 };
 
+// use lettre::transport::smtp::authentication::Credentials;
+use lettre::{
+    Message, 
+    SmtpTransport, 
+    Transport,
+    transport::smtp::authentication::Credentials
+};
+
+
+pub enum MailerError {
+    ConnectionError,
+    SendError
+}
 
 
 pub struct Mailer {
-
+    relay_host: String,
+    credentials: Credentials,
+    transport: Option<SmtpTransport> 
 }
 
 
@@ -18,9 +33,28 @@ impl Mailer {
         smtp_user: &str,
         smtp_password: &str
     ) -> Self {
-        return Self {
+        let creds = Credentials::new(
+            String::from(smtp_user),
+            String::from(smtp_password) 
+        );
 
+        return Self {
+            relay_host: String::from(smtp_host),
+            credentials: creds,
+            transport: None
         };
+    }
+
+
+    pub fn connect(&mut self) -> Result<(), MailerError> {
+        let mailer = SmtpTransport::relay(&self.relay_host)
+            .unwrap()
+            .credentials(self.credentials.clone())
+            .build();
+
+        self.transport = Some(mailer);
+
+        return Ok(());
     }
 
     pub fn send(
@@ -29,8 +63,33 @@ impl Mailer {
         to: &str,
         subject: &str,
         body: &str
-    ) {
+    ) -> Result<(), MailerError> {
+        let email = Message::builder()
+            // .from("NoBody <nobody@domain.tld>".parse().unwrap())
+            // .reply_to("Yuin <yuin@domain.tld>".parse().unwrap())
+            // .to("Hei <hei@domain.tld>".parse().unwrap())
+            // .subject("Happy new year")
+            // .body(String::from("Be happy!"))
+            .from(from.parse().unwrap())
+            .reply_to(to.parse().unwrap())
+            .subject(subject)
+            .body(String::from(body))
+            .unwrap();
 
+        if let Some(transport) = &self.transport {
+            match transport.send(&email) {
+                Err(e) => {
+                    error!("unable to send email: {:?}", e);
+                    return Err(MailerError::SendError);
+                }
+                Ok(_) => {
+                    info!("email sent");
+                    return Ok(());
+                }
+            }
+        } else {
+            return Err(MailerError::SendError);
+        }
     }
 }
 
