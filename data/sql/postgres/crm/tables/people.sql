39be9dce6036363b83bc0cd0e761b05611e97220
beowulf1416 <beowulf1416@gmail.com>
2022-09-04 06:50:08 +0800

    switch from domains to clients. add placeholder for countries and currencies procs.

--- data/sql/postgres/crm/tables/people.sql
+++ data/sql/postgres/crm/tables/people.sql
@@ -1,6 +1,6 @@
 create table people (
     id uuid not null,
-    domain_id uuid not null,
+    client_id uuid not null,
     active boolean not null default false,
     created timestamp without time zone not null default(now() at time zone 'utc'),
 
@@ -12,5 +12,29 @@ create table people (
     suffix varchar(100),
 
     constraint pk_people
-        primary key (id)
+        primary key (id),
+
+    constraint fk_people_1
+        foreign key (client_id)
+        references client.clients (id)
+        on delete restrict
+);
+
+
+create table people_scd (
+    people_id uuid not null,
+    active boolean not null default false,
+    created timestamp without time zone not null default(now() at time zone 'utc'),
+
+    given_name varchar(200) not null,
+    middle_name varchar(200),
+    family_name varchar(200) not null,
+
+    prefix varchar(100),
+    suffix varchar(100),
+
+    constraint fk_people_scd_1
+        foreign key (people_id)
+        references people (id)
+        on delete restrict
 );
