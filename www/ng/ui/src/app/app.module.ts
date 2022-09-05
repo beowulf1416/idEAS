65d043d201354a2faa4e2a9a158a94b83d7be8f4
beowulf1416 <beowulf1416@gmail.com>
2022-09-06 07:21:40 +0800

    import modules

--- www/ng/ui/src/app/app.module.ts
+++ www/ng/ui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { WorkspaceComponent } from './components/workspace/workspace.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule
   ],
