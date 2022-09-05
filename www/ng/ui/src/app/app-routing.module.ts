0af6ba64a6218d37f8357ecf061b413ccb31e6b2
beowulf1416 <beowulf1416@gmail.com>
2022-09-06 06:09:48 +0800

    adding workspace component

--- www/ng/ui/src/app/app-routing.module.ts
+++ www/ng/ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "dashboard",
+    component: WorkspaceComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- www/ng/ui/src/app/app.module.ts
+++ www/ng/ui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
