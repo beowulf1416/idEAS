bac7e8b6e87e8a9b15d4e5bd9ca26cb89c715e56
beowulf1416 <beowulf1416@gmail.com>
2022-09-11 22:22:19 +0800

    add organization add and update sp

--- data/sql/postgres/client/procs.sql
+++ data/sql/postgres/client/procs.sql
@@ -1,5 +1,11 @@
 set schema 'client';
 
+-- clients
 \ir procs/clients/client_add.sql
 \ir procs/clients/client_update.sql
 \ir procs/clients/client_set_active.sql
+
+
+-- organizations
+\ir procs/organizations/organization_add.sql
+\ir procs/organizations/organization_update.sql
---
+++ data/sql/postgres/client/procs/organizations/organization_add.sql
@@ -0,0 +1,23 @@
+create or replace procedure organization_add(
+    p_client_id client.organizations.client_id%type,
+    p_organization_id client.organizations.id%type,
+    p_name client.organizations.name%type,
+    p_description client.organizations.description%type
+)
+language plpgsql
+as $$
+begin
+    insert into client.organizations (
+        id,
+        client_id,
+        active,
+        name,
+        description
+    ) values (
+        p_organization_id,
+        p_client_id,
+        p_name,
+        p_description
+    );
+end
+$$;
