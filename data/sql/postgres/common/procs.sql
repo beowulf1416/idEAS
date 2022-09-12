cacf4cdf061352e9767d2590eb6001d789da933e
beowulf1416 <beowulf1416@gmail.com>
2022-09-13 06:29:15 +0800

    add fetch sp for currencies, dimensions and uom.

--- data/sql/postgres/common/procs.sql
+++ data/sql/postgres/common/procs.sql
@@ -1,3 +1,12 @@
 set schema 'common';
 
 \ir procs/countries/countries_fetch.sql
+
+
+\ir procs/currencies/currencies_fetch.sql
+
+
+\ir procs/dimensions/dimensions_fetch.sql
+
+
+\ir procs/uom/uom_fetch.sql
