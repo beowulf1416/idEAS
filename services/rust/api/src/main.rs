33c5f9c229048c1f53f69d01392eb9f77eabc8a4
beowulf1416 <beowulf1416@gmail.com>
2022-09-14 20:36:17 +0800

    use api and auth sections in config

--- services/rust/api/src/main.rs
+++ services/rust/api/src/main.rs
@@ -1,8 +1,51 @@
 extern crate log;
 
+mod classes;
+mod middleware;
 mod endpoints;
 
 
-fn main() {
-    println!("Hello, world!");
+use log::{
+    info
+};
+
+use actix_web::{
+    App,
+    HttpServer
+};
+
+use config::{
+    get_configuration
+};
+
+#[actix_web::main]
+async fn main()  -> std::io::Result<()> {
+    env_logger::init();
+    info!("starting up ...");
+
+
+    if let Some(config) = get_configuration() {
+        debug!("parsed config: {:?}", config);
+
+        // let bind_host = config.bind_host.clone();
+        // let bind_port = config.bind_port.clone();
+        let bind_host = config.api.bind_host.clone();
+        let bind_port = config.api.bind_port.clone();
+
+        let server = HttpServer::new(move || {
+            App::new()
+                .app_data(web::Data::new(config.clone()))
+                .app_data(pg::Db::new(&config.clone()))
+        })
+        .bind(format!("{}:{}", bind_host, bind_port))?
+        .run();
+
+        info!("Server is listening at https://{}:{}", bind_host, bind_port);
+        return server.await;
+    } else {
+        error!("unable to retrieve configuration");
+    }
+
+    info!("Exiting...");
+    return Ok(());
 }
