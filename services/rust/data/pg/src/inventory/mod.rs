56eadf6fbe2405bc9fea194d2db2cbdf57df62d3
beowulf1416 <beowulf1416@gmail.com>
2022-09-15 21:37:15 +0800

    add function to add inventory item.

---
+++ services/rust/data/pg/src/inventory/mod.rs
@@ -0,0 +1,38 @@
+pub mod item;
+
+
+use log::{
+    info,
+    error,
+    debug
+};
+
+
+use deadpool::managed::Object;
+use deadpool_postgres::{ 
+    Manager
+};
+use tokio_postgres::{
+    error::SqlState
+};
+
+use crate::{
+    DbError,
+    Dbo
+};
+
+
+pub struct Inventory {
+    dbo: Dbo,
+    item: Option<item::Item>
+}
+
+
+impl Inventory {
+    pub fn new(client: Object<Manager>) -> Self {
+        return Self {
+            dbo: Dbo::new(client),
+            item: None
+        };
+    }
+}
