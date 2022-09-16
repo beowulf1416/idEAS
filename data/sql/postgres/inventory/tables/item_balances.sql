964d1be7d17282a216d9a08e3ca98fb6397003b0
beowulf1416 <beowulf1416@gmail.com>
2022-09-16 21:21:19 +0800

    remove references to domains. add sp for item_balances.

--- data/sql/postgres/inventory/tables/item_balances.sql
+++ data/sql/postgres/inventory/tables/item_balances.sql
@@ -1,30 +1,30 @@
 create table item_balances (
-    id uuid not null,
-    domain_id uuid not null,
+    item_id uuid not null,
+    client_id uuid not null,
     active boolean not null default false,
     created timestamp without time zone not null default(now() at time zone 'utc'),
 
     balance decimal(12, 3) not null default 0,
 
     constraint pk_item_balances
-        primary key (id),
+        primary key (item_id),
 
     constraint fk_item_balances_1
-        foreign key (domain_id)
-        references domain.domains (id)
+        foreign key (client_id)
+        references client.clients (id)
         on delete restrict
 );
 
 create table item_balances_history (
-    id uuid not null,
-    domain_id uuid not null,
+    item_id uuid not null,
+    client_id uuid not null,
     active boolean not null default false,
     created timestamp without time zone not null default(now() at time zone 'utc'),
 
     balance decimal(12, 3) not null default 0,
 
     constraint fk_item_balances_history_1
-        foreign key (domain_id)
-        references domain.domains (id)
+        foreign key (client_id)
+        references client.clients (id)
         on delete restrict
 );
