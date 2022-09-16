85967a9ca35e90c83e261c065605a3a2f613816a
beowulf1416 <beowulf1416@gmail.com>
2022-09-16 21:22:14 +0800

    add sp for items

---
+++ data/sql/postgres/inventory/procs/items/item_set_active.sql
@@ -0,0 +1,14 @@
+create or replace procedure item_set_active(
+    p_item_id inventory.items.id%type,
+    p_active = inventory.items.active%type
+)
+language plpgsql
+as $$
+begin
+    update inventory.items set
+        active = p_active
+    where
+        id = p_item_id
+    ;
+end
+$$
