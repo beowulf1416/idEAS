6741b53ed45aecbcb5d2e2766bf9cef90205972d
beowulf1416 <beowulf1416@gmail.com>
2022-09-16 21:09:42 +0800

    remove reference to domain. add sp for item_locations.

---
+++ data/sql/postgres/inventory/procs/item_locations/item_location_add.sql
@@ -0,0 +1,37 @@
+create or replace procedure item_location_add(
+    p_client_id inventory.item_locations.client_id%type,
+    p_item_id inventory.item_locations.item_id%type,
+    p_location_id inventory.item_locations.location_id%type,
+    p_id inventory.item_locations.id%type,
+    p_batch_id inventory.item_locations.batch_id%type,
+    p_lot_id inventory.item_locations.lot_id%type,
+    p_expiry inventory.item_locations.expiry%type,
+    p_balance inventory.item_locations.balance%type,
+    p_uom_id inventory.item_locations.uom_id%type
+)
+language plpgsql
+as $$
+begin
+    insert into inventory.item_locations (
+        id,
+        client_id,
+        item_id,
+        location_id,
+        batch_id,
+        lot_id,
+        expiry,
+        balance,
+        uom_id
+    ) values (
+        p_id,
+        p_client_id,
+        p_item_id,
+        p_location_id,
+        p_batch_id,
+        p_lot_id,
+        p_expiry,
+        p_balance,
+        p_uom_id
+    );
+end
+$$;
--- data/sql/postgres/inventory/tables/item_locations.sql
+++ data/sql/postgres/inventory/tables/item_locations.sql
@@ -1,6 +1,6 @@
 create table item_locations (
     id uuid not null,
-    domain_id uuid not null,
+    client_id uuid not null,
     active boolean not null default false,
     created timestamp without time zone not null default(now() at time zone 'utc'),
 
@@ -19,8 +19,8 @@ create table item_locations (
         primary key (id),
 
     constraint fk_item_locations_1
-        foreign key (domain_id)
-        references domain.domains (id)
+        foreign key (client_id)
+        references client.clients (id)
         on delete restrict
 );
 
