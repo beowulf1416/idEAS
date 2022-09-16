1dc25d9db54aab9d1d83fa8aa7219b7af3dcdd19
beowulf1416 <beowulf1416@gmail.com>
2022-09-16 20:50:08 +0800

    remove references to domain

--- data/sql/postgres/inventory/tables/locations.sql
+++ data/sql/postgres/inventory/tables/locations.sql
@@ -1,6 +1,6 @@
 create table locations (
     id uuid not null,
-    domain_id uuid not null,
+    client_id uuid not null,
     active boolean not null default false,
     created timestamp without time zone not null default(now() at time zone 'utc'),
 
@@ -14,7 +14,7 @@ create table locations (
         primary key (id),
 
     constraint fk_locations_1
-        foreign key (domain_id)
-        references domain.domains (id)
+        foreign key (client_id)
+        references client.clients (id)
         on delete restrict
 );
--- data/sql/postgres/inventory/tables/warehouses.sql
+++ data/sql/postgres/inventory/tables/warehouses.sql
@@ -1,6 +1,6 @@
 create table warehouses (
     id uuid not null,
-    domain_id uuid not null,
+    client_id uuid not null,
     active boolean not null default false,
     created timestamp without time zone not null default(now() at time zone 'utc'),
 
@@ -11,7 +11,7 @@ create table warehouses (
         primary key (id),
 
     constraint fk_warehouses_1
-        foreign key (domain_id)
-        references domain.domains (id)
+        foreign key (client_id)
+        references client.clients (id)
         on delete restrict
 );
