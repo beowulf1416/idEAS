45a36eefb52fd63a37ae188d23e375c3f3083c46
beowulf1416 <beowulf1416@gmail.com>
2022-09-16 20:49:20 +0800

    add sp for locations

---
+++ data/sql/postgres/inventory/procs/locations/location_add.sql
@@ -0,0 +1,31 @@
+create or replace procedure location_add(
+    p_client_id inventory.locations.client_id%type,
+    p_location_id inventory.locations.id%type,
+    p_aisle inventory.locations.aisle%type,
+    p_shelf inventory.locations.shelf%type,
+    p_bin inventory.locations.bin%type,
+    p_level inventory.locations.level%type,
+    p_floor inventory.locations.floor%type
+)
+language plpgsql
+as $$
+begin
+    insert into inventory.locations (
+        id,
+        client_id,
+        aisle,
+        shelf,
+        bin,
+        level,
+        floor
+    ) values (
+        p_location_id,
+        p_client_id,
+        p_aisle,
+        p_shelf,
+        p_bin,
+        p_level,
+        p_floor
+    );
+end
+$$;
