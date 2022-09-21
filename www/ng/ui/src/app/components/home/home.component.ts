27320786475a5bd3c8a307bdb13735d59a523e0c
beowulf1416 <beowulf1416@gmail.com>
2022-09-21 20:29:54 +0800

    modify home. dashboard. registration components.

--- www/ng/ui/src/app/components/home/home.component.ts
+++ www/ng/ui/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TitleService } from 'src/app/services/title.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private title: TitleService
+  ) { 
+    this.title.set_title('Home');
+  }
 
   ngOnInit(): void {
   }
--- www/ng/ui/src/app/modules/dashboard/dashboard.module.ts
+++ www/ng/ui/src/app/modules/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MaterialModule } from 'src/material/material.module';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     DashboardRoutingModule
   ]
 })
