09d494531e4bc9df07d5a12f3e45cd5b5e23c442
beowulf1416 <beowulf1416@gmail.com>
2022-10-05 06:07:13 +0800

    modify registration page

--- www/ng/ui/src/environments/environment.prod.ts
+++ www/ng/ui/src/environments/environment.prod.ts
@@ -7,6 +7,7 @@ export const environment = {
   api_url_registration_base: "http://localhost:8081",
   api_register_start: "/auth/register",
   api_register_complete: "/auth/register/complete",
+  api_registration_info: "/aut/register/info",
 
   // api
   api_url_base: "http://localhost:8082"
