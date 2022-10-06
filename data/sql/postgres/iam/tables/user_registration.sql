3df78320a85bf62e1c32ab8024aa7800932734f0
beowulf1416 <beowulf1416@gmail.com>
2022-10-06 22:08:27 +0800

    remove token field

--- data/sql/postgres/iam/tables/user_registration.sql
+++ data/sql/postgres/iam/tables/user_registration.sql
@@ -2,7 +2,6 @@ create table user_registration (
     id uuid not null,
     created timestamp without time zone not null default(now() at time zone 'utc'),
 
-    token varchar(20) not null,
     email common.email_address,
 
     expires timestamp without time zone not null default(now() at time zone 'utc' + interval '1' day),
