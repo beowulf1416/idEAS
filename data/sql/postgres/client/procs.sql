8301350c828bd2517ed0f8ad097b0380c6ef54ca
beowulf1416 <beowulf1416@gmail.com>
2022-10-06 20:31:24 +0800

    add client fetch sp

--- data/sql/postgres/client/procs.sql
+++ data/sql/postgres/client/procs.sql
@@ -4,6 +4,7 @@ set schema 'client';
 \ir procs/clients/client_add.sql
 \ir procs/clients/client_update.sql
 \ir procs/clients/client_set_active.sql
+\ir procs/clients/client_fetch.sql
 
 
 -- organizations
