ff7a76e357512881a650446b31f6e5113b5ae1a9
beowulf1416 <beowulf1416@gmail.com>
2022-10-07 09:35:18 +0800

    add endpoint to retrieve current user

--- services/rust/api/src/endpoints/user.rs
+++ services/rust/api/src/endpoints/user.rs
@@ -25,3 +25,40 @@ use pg::{
 };
 
 
+#[derive(Debug, Serialize, Deserialize)]
+struct CurrentUserRequest {
+}
+
+
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg
+        .service(
+            web::resource("current")
+                .route(web::method(http::Method::OPTIONS).to(default_options))
+                .route(web::get().to(user_current_get))
+                .route(web::post().to(user_current_post))
+        )
+    ;
+}
+
+
+async fn user_current_get() -> impl Responder {
+    info!("user_current_get()");
+    return HttpResponse::Ok().body("use POST method instead");
+}
+
+
+async fn user_current_post(
+    db: web::Data<Db>,
+    params: web::Json<CurrentUserRequest>
+) -> impl Responder {
+    info!("user_current_post()");
+
+    return HttpResponse::Ok()
+        .json(ApiResponse::new(
+            false,
+            String::from("Service is up. version: 1.0.0.0.dev"),
+            None
+        ));
+}
