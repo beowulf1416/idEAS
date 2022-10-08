e36cedf027d05b3f2d80c3664d8733aa0363c09e
beowulf1416 <beowulf1416@gmail.com>
2022-10-08 09:15:12 +0800

    remove client_id from people table. add client_people table.

--- data/sql/postgres/crm/tables.sql
+++ data/sql/postgres/crm/tables.sql
@@ -5,3 +5,5 @@ set schema 'crm';
 \ir tables/identifier_types.sql
 \ir tables/people.sql
 \ir tables/identifiers.sql
+
+\ir tables/client_people.sql
---
+++ data/sql/postgres/crm/tables/client_people.sql
@@ -0,0 +1,16 @@
+create table client_people(
+    client_id uuid not null,
+    people_id uuid not null,
+
+    constraint pk_client_people
+        primary key (client_id, people_id),
+
+    constraint fk_client_people_1
+        foreign key (client_id)
+        references client.clients (id)
+        on delete restrict,
+    constraint fk_client_people_2
+        foreign key (people_id)
+        references crm.people (id)
+        on delete restrict
+);
