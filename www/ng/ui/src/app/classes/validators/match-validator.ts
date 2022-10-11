765fadb48f88cde83dd35fa0ec8e1b2010c8e7b6
beowulf1416 <beowulf1416@gmail.com>
2022-10-11 16:22:22 +0800

    add sign up module

--- www/ng/ui/src/app/classes/validators/match-validator.ts
+++ www/ng/ui/src/app/classes/validators/match-validator.ts
@@ -4,7 +4,7 @@ export function matchValidator(control1: string, control2: string): ValidatorFn 
     return (control: AbstractControl): ValidationErrors | null => {
       const value1 = control.get(control1)?.value;
       const value2 = control.get(control2)?.value;
-  
+
       return value1 == value2 ? null : { identical: true };
     }
 }
