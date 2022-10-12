4a122b0845427a2a93b19923f33120507100ac13
beowulf1416 <beowulf1416@gmail.com>
2022-10-12 22:16:19 +0800

    modify auth interceptor - INCOMPLETE

--- www/ng/ui/src/app/interceptors/auth.interceptor.ts
+++ www/ng/ui/src/app/interceptors/auth.interceptor.ts
@@ -3,9 +3,10 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { finalize, Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
@@ -17,15 +18,30 @@ export class AuthInterceptor implements HttpInterceptor {
     console.log("AuthInterceptor::intercept()");
     const token = sessionStorage.getItem(environment.session_token_key);
 
-    if (token != null) {
-        console.log("AuthInterceptor::intercept() Adding auth token");
-        const authReq = request.clone({
-            setHeaders: { Authorization: `Bearer ${token}` }
-        });
+    const authReq = request.clone({
+      setHeaders: { Authorization: `Bearer ${token}` }
+    });
+  
+    return next.handle(authReq)
+      .pipe(
+        tap({
+          next: (e) => {
+            console.log("AuthInterceptor::intercept()", e);
+            console.log(e instanceof HttpResponse);
+          }
+        })
+      );
 
-        return next.handle(authReq);
-    } else {
-        return next.handle(request);
-    }
+
+    // if (token != null) {
+    //     console.log("AuthInterceptor::intercept() Adding auth token");
+    //     const authReq = request.clone({
+    //         setHeaders: { Authorization: `Bearer ${token}` }
+    //     });
+
+    //     return next.handle(authReq);
+    // } else {
+    //     return next.handle(request);
+    // }
   }
 }
