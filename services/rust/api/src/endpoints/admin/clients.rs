2138603a63c276070526b94f1af2a22320cb0e1a
beowulf1416 <beowulf1416@gmail.com>
2022-10-16 22:39:48 +0800

    add /clients/fetch endpoint. add test.

--- services/rust/api/src/endpoints/admin/clients.rs
+++ services/rust/api/src/endpoints/admin/clients.rs
@@ -26,3 +26,49 @@ use pg::{
 };
 
 
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ClientAddRequest {
+    id: uuid::Uuid,
+    name: String,
+    description: String,
+    address: String,
+    country_id: i32,
+    url: String
+}
+
+
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    /// base url /clients
+    cfg
+        .service(
+            web::resource("add")
+                .route(web::method(http::Method::OPTIONS).to(default_options))
+                .route(web::get().to(client_add_get))
+                .route(web::post().to(client_add_post))
+        )
+    ;
+}
+
+
+
+async fn client_add_get() -> impl Responder {
+    info!("client_add_get()");
+    return HttpResponse::Ok().body("use POST method instead");
+}
+
+
+async fn client_add_post(
+    db: web::Data<Db>,
+    params: web::Json<ClientAddRequest>
+) -> impl Responder {
+    info!("client_add_post()");
+
+    return HttpResponse::Ok()
+        .json(ApiResponse::new(
+            false,
+            String::from("Service is up. version: 1.0.0.0.dev"),
+            None
+        ));
+}
