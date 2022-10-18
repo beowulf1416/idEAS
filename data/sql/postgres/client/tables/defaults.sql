4fb55a559368acd4e1664aa049c85a81177c4645
beowulf1416 <beowulf1416@gmail.com>
2022-10-18 21:02:35 +0800

    modify client schema

--- data/sql/postgres/client/tables/defaults.sql
+++ data/sql/postgres/client/tables/defaults.sql
@@ -1,7 +1,7 @@
 create table defaults (
     client_id uuid not null,
 
-    currency_id char(3) not null,
+    currency_id varchar(10) not null,
 
     constraint pk_defaults
         primary key (client_id),
@@ -9,5 +9,9 @@ create table defaults (
     constraint fk_defaults_1
         foreign key (client_id)
         references client.clients (id)
+        on delete restrict,
+    constraint fk_defaults_2
+        foreign key (currency_id)
+        references common.currencies (currency)
         on delete restrict
 );
