b023e21c32c4fc84b76f2f11988214d6787928fa
beowulf1416 <beowulf1416@gmail.com>
2022-10-19 22:47:06 +0800

    modify fetching from /countries/fetch endpoint

--- www/ng/ui/src/app/services/country.service.ts
+++ www/ng/ui/src/app/services/country.service.ts
@@ -15,26 +15,40 @@ export class CountryService {
   ) { }
 
   fetch(): Observable<Array<Country>> {
+    console.log("CountryService::fetch()");
     let self = this;
-    return new Observable<Array<Country>>(function(observer) {
-      self.http.post<ApiResponse>(
-        environment.api_url_base + environment.api_country_fetch,
-        {}
-      ).pipe(
-        filter(r => r.success),
-        map(r => {
-          let countries = (r.data as { countries: Array<Country> }).countries;
-          observer.next(countries);
-        }),
-        catchError(e => {
-          console.error("CountryService::fetch()", e);
-          // observer.next(new Array<Country>());
-          return new Array<Country>();
-        }),
-        finalize(() => {
-          observer.complete();
-        })
-      );
-    });
+    // return new Observable<Array<Country>>(function(observer) {
+    //   self.http.post<ApiResponse>(
+    //     environment.api_url_base + environment.api_country_fetch,
+    //     {}
+    //   ).pipe(
+    //     filter(r => r.success),
+    //     map(r => {
+    //       console.debug("CountryService::fetch()", r);
+    //       let countries = (r.data as { countries: Array<Country> }).countries;
+    //       observer.next(countries);
+    //     }),
+    //     catchError(e => {
+    //       console.error("CountryService::fetch()", e);
+    //       // observer.next(new Array<Country>());
+    //       return new Array<Country>();
+    //     }),
+    //     finalize(() => {
+    //       observer.complete();
+    //     })
+    //   );
+    // });
+
+    return this.http.post<ApiResponse>(
+      environment.api_url_base + environment.api_country_fetch,
+      {}
+    ).pipe(
+      filter(r => r.success),
+      map(r => {
+        console.debug(r);
+        let countries = (r.data as { countries: Array<Country> }).countries;
+        return countries; 
+      })
+    );
   }
 }
