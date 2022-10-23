bc70613dfd85913a47f600bc5e7619f4c7d6dcaa
beowulf1416 <beowulf1416@gmail.com>
2022-10-23 09:42:15 +0800

    adding client join router link.

---
+++ www/ng/ui/src/app/modules/client/components/client/client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.css']
+})
+export class ClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
