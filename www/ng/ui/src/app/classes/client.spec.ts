b7ab1e3993f2d807fffbf5789f47f90ffa3cc733
beowulf1416 <beowulf1416@gmail.com>
2022-10-24 22:51:32 +0800

    modify route and components for viewing editing client records.

---
+++ www/ng/ui/src/app/classes/client.spec.ts
@@ -0,0 +1,7 @@
+import { Client } from './client';
+
+describe('Client', () => {
+  it('should create an instance', () => {
+    expect(new Client()).toBeTruthy();
+  });
+});
---
+++ www/ng/ui/src/app/classes/client.ts
@@ -0,0 +1,9 @@
+export interface Client {
+    id: string,
+    name: string,
+    description: string,
+    address: string,
+    active: boolean,
+    country_id: number,
+    url: string
+}
