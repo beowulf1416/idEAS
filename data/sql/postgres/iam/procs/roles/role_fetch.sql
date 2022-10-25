610667193a86294394695045a1a5f8d9c5dc1033
beowulf1416 <beowulf1416@gmail.com>
2022-10-25 20:03:19 +0800

    modify iam schema

--- data/sql/postgres/iam/procs/roles/role_fetch.sql
+++ data/sql/postgres/iam/procs/roles/role_fetch.sql
@@ -1,15 +1,14 @@
 create or replace function role_fetch(
-    p_domain_id iam.roles.client_id%type,
+    p_client_id iam.roles.client_id%type,
     p_filter varchar(100),
     p_items int default 10,
     p_page int default 0
 )
 returns table (
     id iam.roles.id%type,
-    client_id iam.roles.client_id%type,
     active iam.roles.active%type,
     name iam.roles.name%type,
-    slug iam.roles.slug%type
+    description iam.roles.description%type
 )
 language plpgsql
 as $$
@@ -18,11 +17,12 @@ begin
         return query
         select
             a.id,
-            a.client_id,
             a.active,
             a.name,
-            a.slug
+            a.description
         from iam.roles a
+        where
+            a.client_id = p_client_id
         order by
             a.name
         limit p_items
@@ -32,15 +32,15 @@ begin
         return query
         select
             a.id,
-            a.client_id,
             a.active,
             a.name,
-            a.slug
+            a.description
         from iam.roles a
         where
-            concat(
+            a.client_id = p_client_id
+            and concat(
                 a.name,
-                a.slug
+                a.description
             ) like p_filter
         order by
             a.name
