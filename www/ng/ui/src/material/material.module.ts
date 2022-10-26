4588e9142e76eb918a2031c104625fdbfa61a062
beowulf1416 <beowulf1416@gmail.com>
2022-10-26 20:09:14 +0800

    add snackbar to display notifications. user message service in user service notifications.

--- www/ng/ui/src/material/material.module.ts
+++ www/ng/ui/src/material/material.module.ts
@@ -16,6 +16,7 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [],
@@ -23,21 +24,22 @@ import {MatSelectModule} from '@angular/material/select';
     CommonModule
   ],
   exports: [
-      MatFormFieldModule,
-      MatInputModule,
-      MatCheckboxModule,
-      MatButtonModule,
-      MatIconModule,
-      MatToolbarModule,
-      MatSidenavModule,
-      MatListModule,
-      MatExpansionModule,
-      MatMenuModule,
-      MatBadgeModule,
-      MatSlideToggleModule,
-      MatPaginatorModule,
-      MatDividerModule,
-      MatSelectModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatBadgeModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatSnackBarModule
   ]
 })
 export class MaterialModule { }
