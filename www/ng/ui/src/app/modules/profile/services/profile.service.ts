23bc58a5f6407f418a29029b2d7f597be816358d
beowulf1416 <beowulf1416@gmail.com>
2022-10-29 22:25:09 +0800

    modify app component

--- www/ng/ui/src/app/modules/profile/services/profile.service.ts
+++ www/ng/ui/src/app/modules/profile/services/profile.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from 'src/app/classes/api-response';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,16 @@ export class ProfileService {
   constructor(
     private http: HttpClient
   ) { }
+
+  update(
+    given_name: string,
+    middle_name: string,
+    family_name: string,
+    prefix: string,
+    suffix: string
+  ): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(
+      
+    );
+  }
 }
