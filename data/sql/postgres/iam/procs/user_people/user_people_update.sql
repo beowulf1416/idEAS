14812c466beca6493eae578fae779c83773a22b6
beowulf1416 <beowulf1416@gmail.com>
2022-10-29 21:24:14 +0800

    modify people procs

--- data/sql/postgres/iam/procs/user_people/user_people_update.sql
+++ data/sql/postgres/iam/procs/user_people/user_people_update.sql
@@ -1,17 +1,46 @@
 create or replace procedure user_people_update(
     p_user_id iam.user_people.user_id%type,
-    p_people_id iam.user_people.people_id%type
+    p_people_id iam.user_people.people_id%type,
+    p_given_name crm.people.given_name%type,
+    p_middle_name crm.people.middle_name%type,
+    p_family_name crm.people.family_name%type,
+    p_prefix crm.people.prefix%type,
+    p_suffix crm.people.suffix%type
 )
 language plpgsql
 as $$
+declare
+    t_people_id iam.user_people.people_id%type;
 begin
-    insert into iam.user_people (
-        user_id,
-        people_id
-    ) values (
-        p_user_id,
-        p_people_id
-    );
+    t_people_id := iam.user_get_people_id(p_user_id);
+
+    if t_people_id = p_people_id then
+        call crm.people_update(
+            p_people_id,
+            p_given_name,
+            p_middle_name,
+            p_family_name,
+            p_prefix,
+            p_suffix
+        );
+    else
+        call crm.people_add(
+            p_people_id,
+            p_given_name,
+            p_middle_name,
+            p_family_name,
+            p_prefix,
+            p_suffix
+        );
+
+        insert into iam.user_people (
+            user_id,
+            people_id
+        ) values (
+            p_user_id,
+            p_people_id
+        );
+    end if;
 end
 $$;
 
