2e5949a9398bb864b378a5feb7adcf8a69f6201d
beowulf1416 <beowulf1416@gmail.com>
2022-11-01 20:18:09 +0800

    add common toolbar component

---
+++ www/ng/ui/src/app/modules/admin/modules/client/components/client-toolbar/client-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-toolbar',
+  templateUrl: './client-toolbar.component.html',
+  styleUrls: ['./client-toolbar.component.css']
+})
+export class ClientToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
