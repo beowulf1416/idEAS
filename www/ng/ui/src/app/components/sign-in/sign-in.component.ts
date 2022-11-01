9d2852ca40d7de81e440f733058b81752d1462d3
beowulf1416 <beowulf1416@gmail.com>
2022-11-01 20:14:19 +0800

    add check in user service. modify sign in component.

--- www/ng/ui/src/app/components/sign-in/sign-in.component.ts
+++ www/ng/ui/src/app/components/sign-in/sign-in.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MessageType } from 'src/app/classes/message-type';
+import { MessageService } from 'src/app/services/message.service';
 import { TitleService } from 'src/app/services/title.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -24,12 +27,21 @@ export class SignInComponent implements OnInit {
     ])
   });
 
+  messages = '';
+  msg_subscription: Subscription;
+
   constructor(
     private title: TitleService,
     private user_service: UserService,
+    private msg_service: MessageService,
     private router: Router
   ) {
     this.title.set_title("Sign In");
+
+
+    this.msg_subscription = this.msg_service.message$.subscribe(r => {
+        this.messages = r.message;
+    });
   }
 
   ngOnInit(): void {
@@ -55,6 +67,9 @@ export class SignInComponent implements OnInit {
         if (r.success) {
           console.log("redirecting...");
           this.router.navigate([""]);
+        } else {
+          console.error("SignInComponent::submit()", r);
+          this.msg_service.send(r.message, MessageType.error);
         }
       })
     }
