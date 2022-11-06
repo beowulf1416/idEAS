00e56d9de143544c0b27f2c6a7508dc3444bd269
beowulf1416 <beowulf1416@gmail.com>
2022-11-06 15:17:25 +0800

    add role permissions component

---
+++ www/ng/ui/src/app/modules/admin/modules/roles/components/role-permissions/role-permissions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-role-permissions',
+  templateUrl: './role-permissions.component.html',
+  styleUrls: ['./role-permissions.component.css']
+})
+export class RolePermissionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
