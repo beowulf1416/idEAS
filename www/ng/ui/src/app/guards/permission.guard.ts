9dd51e80317902d33cd16746ab9604ff07709c7f
beowulf1416 <beowulf1416@gmail.com>
2022-11-07 05:52:15 +0800

    add permission guard. modify route to use permission guard.

---
+++ www/ng/ui/src/app/guards/permission.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissionGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      console.debug("PermissionGuard::canActivate()", route.data['permission']);
+    return true;
+  }
+  
+}
