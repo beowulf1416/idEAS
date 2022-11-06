5f5fcce25c3b46c6384a9f9c353f960efe9ae81b
beowulf1416 <beowulf1416@gmail.com>
2022-11-06 14:55:29 +0800

    modify inventory module

--- www/ng/ui/src/app/modules/inventory/inventory.module.ts
+++ www/ng/ui/src/app/modules/inventory/inventory.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/material/material.module';
 
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ItemComponent } from './components/item/item.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { InventoryToolbarComponent } from './components/inventory-toolbar/inventory-toolbar.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ItemComponent,
-    ItemListComponent
+    ItemListComponent,
+    CategoryComponent,
+    CategoriesComponent,
+    InventoryToolbarComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    MaterialModule,
     InventoryRoutingModule
   ]
 })
