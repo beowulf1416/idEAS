75fca1d158bdfbbfb9da86cad03fdc16a5b0ea92
beowulf1416 <beowulf1416@gmail.com>
2022-11-07 21:25:45 +0800

    add permission service

---
+++ www/ng/ui/src/app/modules/admin/modules/client/services/permission.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from 'src/app/classes/api-response';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissionService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  assigned(
+    role_id: string
+  ) {
+    this.http.post<ApiResponse>(
+      environment.api_url_base + environment.api_permissions_assigned,
+      {
+        role_id: role_id
+      }
+    ).subscribe(r => {
+      console.debug("PermissionService::assigned()", r);
+    });
+  }
+}
