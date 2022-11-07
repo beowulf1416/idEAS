273462defa9a56568da7cba43af7c90ffd59b847
beowulf1416 <beowulf1416@gmail.com>
2022-11-07 21:46:51 +0800

    add functions to retrieve role permissions

--- services/rust/data/pg/src/iam/permission.rs
+++ services/rust/data/pg/src/iam/permission.rs
@@ -58,4 +58,70 @@ impl Permissions {
             }
         }
     }
+
+    pub async fn get_role_permissions(
+        &self,
+        role_id: &uuid::Uuid
+    ) -> Result<Vec<common::iam::permission::Permission>, DbError> {
+        let sql = "select * from iam.role_permissions_get($1);";
+        match self.0.client.prepare_cached(sql).await {
+            Err(e) => {
+                error!("unable to prepare sql: {:?}", e);
+                return Err(DbError::ClientError);
+            }
+            Ok(stmt) => {
+                match self.0.client.query(
+                    &stmt,
+                    &[
+                        &user_id
+                    ]
+                ).await {
+                    Err(e) => {
+                        error!("unable to retrieve permissions: {:?}", e);
+                        return Err(DbError::ClientError);
+                    }
+                    Ok(rows) => {
+                        let results = rows.iter().map(|r| common::iam::permission::Permission {
+                            name: r.get("name")
+                        })
+                        .collect();
+                        return Ok(results);
+                    }
+                }
+            }
+        }
+    }
+
+    pub async fn get_role_permissions_not_assigned(
+        &self,
+        role_id: &uuid::Uuid
+    ) -> Result<Vec<common::iam::permission::Permission>, DbError> {
+        let sql = "select * from iam.role_permissions_not_assigned($1);";
+        match self.0.client.prepare_cached(sql).await {
+            Err(e) => {
+                error!("unable to prepare sql: {:?}", e);
+                return Err(DbError::ClientError);
+            }
+            Ok(stmt) => {
+                match self.0.client.query(
+                    &stmt,
+                    &[
+                        &user_id
+                    ]
+                ).await {
+                    Err(e) => {
+                        error!("unable to retrieve permissions: {:?}", e);
+                        return Err(DbError::ClientError);
+                    }
+                    Ok(rows) => {
+                        let results = rows.iter().map(|r| common::iam::permission::Permission {
+                            name: r.get("name")
+                        })
+                        .collect();
+                        return Ok(results);
+                    }
+                }
+            }
+        }
+    }
 }
