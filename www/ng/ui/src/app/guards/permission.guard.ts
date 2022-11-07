fa8ecd22e5eb81774a560eff284ec449e50c74ed
beowulf1416 <beowulf1416@gmail.com>
2022-11-07 18:51:40 +0800

    modify permission guard

--- www/ng/ui/src/app/guards/permission.guard.ts
+++ www/ng/ui/src/app/guards/permission.guard.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
+import { MessageService } from '../services/message.service';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermissionGuard implements CanActivate {
+
+  constructor(
+    private user_service: UserService,
+    private msg_service: MessageService
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      console.debug("PermissionGuard::canActivate()", route.data['permission']);
-    return true;
+
+    return this.user_service.get_user$().pipe(
+      map(r => {
+        const found = r.permissions.find(x => x == route.data['permission']);
+        console.log("PermissionGuard::canActivate()", found);
+        return found != undefined;
+      })
+    );
   }
   
 }
