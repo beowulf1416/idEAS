5769c5c91eaff0535e1da3e11881d4405b02c43a
beowulf1416 <beowulf1416@gmail.com>
2022-11-08 22:24:12 +0800

    fix typo in role permissions. add sp to retrieve client members.

---
+++ data/sql/postgres/iam/procs/user_clients/user_client_members_fetch.sql
@@ -0,0 +1,25 @@
+create or replace function user_client_members_fetch(
+    p_client_id iam.user_clients.client_id%type
+)
+returns table (
+    id iam.users.id%type,
+    active iam.users.active%type,
+    email iam.users.email%type
+)
+language plpgsql
+as $$
+begin
+    return query
+    select
+        a.id,
+        a.active,
+        a.email
+    from iam.users a
+        inner join iam.user_clients b
+            on a.id = b.user_id
+    where
+        b.client_id = p_client_id
+        and b.active = true
+    ;
+end
+$$;
