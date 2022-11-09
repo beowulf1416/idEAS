fd1589642a4e43a846bf0c538dc55de1b9ce4372
beowulf1416 <beowulf1416@gmail.com>
2022-11-10 05:47:57 +0800

    add components

---
+++ www/ng/ui/src/app/modules/admin/modules/client/components/client-dashboard/client-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-dashboard',
+  templateUrl: './client-dashboard.component.html',
+  styleUrls: ['./client-dashboard.component.css']
+})
+export class ClientDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
