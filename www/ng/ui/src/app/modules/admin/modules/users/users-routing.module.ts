9f96dfa7ecb2779e8d269e4149244fceb5755629
beowulf1416 <beowulf1416@gmail.com>
2022-11-09 22:37:48 +0800

    add user component

--- www/ng/ui/src/app/modules/admin/modules/users/users-routing.module.ts
+++ www/ng/ui/src/app/modules/admin/modules/users/users-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   {
     path: "",
-    component: AdminComponent
+    component: AdminComponent,
+    children: [
+      {
+        path: 'user',
+        component: UserComponent
+      }
+    ]
   }
 ];
 
