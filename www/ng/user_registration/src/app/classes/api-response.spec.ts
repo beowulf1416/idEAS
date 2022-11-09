ae4c14e3031e0699d095eea36fc65a5de1498857
beowulf1416 <beowulf1416@gmail.com>
2022-11-10 05:50:33 +0800

    remove user regisration frontend

--- www/ng/user_registration/src/app/classes/api-response.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ApiResponse } from './api-response';
-
-describe('ApiResponse', () => {
-  it('should create an instance', () => {
-    expect(new ApiResponse()).toBeTruthy();
-  });
-});
--- www/ng/user_registration/src/app/classes/api-response.ts
+++
@@ -1,5 +0,0 @@
-export interface ApiResponse {
-    success: boolean,
-    message: string,
-    data: any
-}
