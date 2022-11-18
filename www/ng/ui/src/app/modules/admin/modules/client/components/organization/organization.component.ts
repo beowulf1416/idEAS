0fa51c2d4b352d16fbf1b613a05642716f214aee
beowulf1416 <beowulf1416@gmail.com>
2022-11-18 21:14:03 +0800

    add organization component

---
+++ www/ng/ui/src/app/modules/admin/modules/client/components/organization/organization.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TitleService } from 'src/app/services/title.service';
+
+@Component({
+  selector: 'app-organization',
+  templateUrl: './organization.component.html',
+  styleUrls: ['./organization.component.css']
+})
+export class OrganizationComponent implements OnInit {
+
+
+  formOrg = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    description: new FormControl('', [])
+  });
+
+  constructor(
+    private title: TitleService
+  ) {
+    this.title.set_title("Organization");
+  }
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+    console.log("OrganizationComponent::submit()");
+  }
+}
