462ce065a7ee8fa68c139225906bdf57c986c833
beowulf1416 <beowulf1416@gmail.com>
2022-11-19 09:50:22 +0800

    upgrade material modules

--- www/ng/ui/src/material/material.module.ts
+++ www/ng/ui/src/material/material.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
-import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
-import {MatLegacyCheckboxModule as MatCheckboxModule} from '@angular/material/legacy-checkbox';
-import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+// import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
+// import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
+// import {MatLegacyCheckboxModule as MatCheckboxModule} from '@angular/material/legacy-checkbox';
+// import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
+// import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatLegacyListModule as MatListModule} from '@angular/material/legacy-list';
+// import {MatLegacyListModule as MatListModule} from '@angular/material/legacy-list';
 import {MatExpansionModule} from '@angular/material/expansion';
-import {MatLegacyMenuModule as MatMenuModule} from '@angular/material/legacy-menu';
+// import {MatLegacyMenuModule as MatMenuModule} from '@angular/material/legacy-menu';
 import {MatBadgeModule} from '@angular/material/badge';
-import {MatLegacySlideToggleModule as MatSlideToggleModule} from '@angular/material/legacy-slide-toggle';
-import {MatLegacyPaginatorModule as MatPaginatorModule} from '@angular/material/legacy-paginator';
+// import {MatLegacySlideToggleModule as MatSlideToggleModule} from '@angular/material/legacy-slide-toggle';
+// import {MatLegacyPaginatorModule as MatPaginatorModule} from '@angular/material/legacy-paginator';
 import {MatDividerModule} from '@angular/material/divider';
-import {MatLegacySelectModule as MatSelectModule} from '@angular/material/legacy-select';
-import {MatLegacySnackBarModule as MatSnackBarModule} from '@angular/material/legacy-snack-bar';
+// import {MatLegacySelectModule as MatSelectModule} from '@angular/material/legacy-select';
+// import {MatLegacySnackBarModule as MatSnackBarModule} from '@angular/material/legacy-snack-bar';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
@@ -39,7 +52,8 @@ import {MatLegacySnackBarModule as MatSnackBarModule} from '@angular/material/le
     MatPaginatorModule,
     MatDividerModule,
     MatSelectModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatTableModule
   ]
 })
 export class MaterialModule { }
