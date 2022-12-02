d0208b0e5127b7571a7346736c9e3fca49afe807
beowulf1416 <beowulf1416@gmail.com>
2022-12-02 21:55:51 +0800

    modify role list component

--- www/ng/ui/src/app/modules/admin/modules/client/components/role-list/role-list.component.ts
+++ www/ng/ui/src/app/modules/admin/modules/client/components/role-list/role-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MessageType } from 'src/app/classes/message-type';
+import { MessageService } from 'src/app/services/message.service';
 import { TitleService } from 'src/app/services/title.service';
 import { RoleService } from '../../services/role.service';
 
@@ -21,10 +23,12 @@ export class RoleListComponent implements OnInit {
 
   client_id = '';
   roles: Array<Role> = new Array<Role>();
+  roles_filter: Array<Role> = new Array<Role>();
 
   constructor(
     private title: TitleService,
     private role_service: RoleService,
+    private message_service: MessageService,
     private route: ActivatedRoute
   ) {
     this.title.set_title("Roles");
@@ -39,7 +43,11 @@ export class RoleListComponent implements OnInit {
       100,
       0
     ).subscribe(r => {
-      console.log(r);
+      if (r.success) {
+        this.roles = (r.data as { roles: Array<Role> }).roles;
+      } else {
+        this.message_service.send(r.message, MessageType.error);
+      }
     });
   }
 }
