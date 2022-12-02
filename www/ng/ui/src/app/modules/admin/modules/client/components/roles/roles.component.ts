034a8202ed987cfffebe01022965a33e5f3fbf5c
beowulf1416 <beowulf1416@gmail.com>
2022-12-03 05:02:31 +0800

    add role component

---
+++ www/ng/ui/src/app/modules/admin/modules/client/components/roles/roles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  templateUrl: './roles.component.html',
+  styleUrls: ['./roles.component.css']
+})
+export class RolesComponent {
+
+}
