958ba2d12d5383cba5fa9836a862109b1c71908c
beowulf1416 <beowulf1416@gmail.com>
2022-12-13 19:22:56 +0800

    add /item/update endpoint

--- services/rust/api/src/endpoints/inventory/item/item.rs
+++ services/rust/api/src/endpoints/inventory/item/item.rs
@@ -8,6 +8,7 @@ use serde::{
     Serialize,
     Deserialize
 };
+use serde_json::json;
 
 use actix_web::{
     HttpResponse, 
@@ -26,6 +27,24 @@ use pg::{
 };
 
 
+#[derive(Debug, Serialize, Deserialize)]
+struct ItemRequest {
+    pub id: uuid::Uuid,
+    pub active: bool,
+    pub name: String,
+    pub description: String,
+    
+    pub sku: String,
+    pub upc: String,
+
+    pub is_perishable: bool,
+    pub is_stocked: bool,
+    pub is_purchased: bool,
+    pub is_sold: bool,
+    pub is_manufactured: bool
+}
+
+
 pub fn config(cfg: &mut web::ServiceConfig) {
     /// base url /clients
     cfg
@@ -34,6 +53,12 @@ pub fn config(cfg: &mut web::ServiceConfig) {
                 .route(web::method(http::Method::OPTIONS).to(default_options))
                 .route(web::get().to(item_add_get))
                 .route(web::post().to(item_add_post))
+        )
+        .service(
+            web::resource("update")
+                .route(web::method(http::Method::OPTIONS).to(default_options))
+                .route(web::get().to(item_update_get))
+                .route(web::post().to(item_update_post))
         )
     ;
 }
@@ -45,7 +70,8 @@ async fn item_add_get() -> impl Responder {
 }
 
 async fn item_add_post(
-    db: web::Data<Db>
+    db: web::Data<Db>,
+    params: web::Json<ItemRequest>
 ) -> impl Responder {
     info!("item_add_post()");
 
@@ -57,5 +83,25 @@ async fn item_add_post(
             error_message,
             None
         ));
+}
 
+async fn item_update_get() -> impl Responder {
+    info!("item_update_get()");
+    return HttpResponse::Ok().body("use POST method instead");
+}
+
+async fn item_update_post(
+    db: web::Data<Db>,
+    params: web::Json<ItemRequest>
+) -> impl Responder {
+    info!("item_update_post()");
+
+    let error_message = String::from("an error occured while trying to process this request");
+
+    return HttpResponse::InternalServerError()
+        .json(ApiResponse::new(
+            false,
+            error_message,
+            None
+        ));
 }
